c007770e03080f6c406ed3b7506e2e2e3ad5c9d2
Julio Corbaz <julio.corbaz@gmail.com>
2024-11-03 20:23:08 -0300

    script www

--- frontend/dist/main.js
+++ frontend/dist/main.js
@@ -1,5 +1,4 @@
-// frontend/src/templates/Header.ts
-var htmlContent = `<div>
+var t=`<div>
   <div class="max-w-md mx-auto bg-green-400 rounded-xl shadow-md overflow-hidden md:max-w-2xl m-32">
     <div class="md:flex">
       <div class="md:shrink-0">
@@ -20,18 +19,4 @@ var htmlContent = `<div>
   <div class="m-6 p-3 shadow-lg text-xl text-center text-purple-300 bg-slate-500 rounded-md">
     Argentina - Buenos Aires - Mar del Plata
   </div>
-</div>`;
-
-// frontend/src/components/Header.tsx
-function Header() {
-  const template = document.createElement("template");
-  template.innerHTML = htmlContent;
-  return template.content;
-}
-var Header_default = Header;
-
-// frontend/src/main.ts
-var app = document.getElementById("app");
-if (app) {
-  app.appendChild(Header_default());
-}
+</div>`;function s(){let e=document.createElement("template");return e.innerHTML=t,e.content}var d=s;var l=document.getElementById("app");if(l)l.appendChild(d());
